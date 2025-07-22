6bfccde087c9ad2ef0303a3a8a9f78e9819a7156
AllenGleeson <allen_gleeson@hotmail.com>
2025-07-22 03:21:09 +0100

    Added Binary Search to Sort Search Class.

--- SortSearchClass.java
+++ SortSearchClass.java
@@ -47,4 +47,17 @@ public class SortSearchClass<T extends Comparable<? super T>> {
 
         return i + 1;
     }
+
+    // Binary Search
+    public int binarySearch(T[] array, T target) {
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int cmp = array[mid].compareTo(target);
+            if (cmp == 0) return mid;
+            if (cmp < 0) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
 }
